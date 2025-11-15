1ab39b0ffdd13c7ea2a23e24830acd148cd7c909
MUHAMMAD HUSSAIN <bilalbushra789@gmail.com>
2025-11-15 17:44:38 +0500

    Q5: Payment details with staff

--- Assignment3_Q5.R
+++ Assignment3_Q5.R
@@ -28,12 +28,21 @@ inventory <- as.data.table(dbReadTable(con, "inventory"))
 rental    <- as.data.table(dbReadTable(con, "rental"))
 address   <- as.data.table(dbReadTable(con, "address"))
 city      <- as.data.table(dbReadTable(con, "city"))
-
-#Q3
-names(language)
-q3_counts <- film[, .(num_films = .N), by = language_id]
-q3_counts
-q3_result <- q3_counts[language, on = "language_id"]
-q3_result
-
+#Q5
+q5 <- payment[
+  staff,
+  on = "staff_id",
+  .(
+    payment_id,
+    amount,
+    payment_date,
+    staff_name = paste(i.first_name, i.last_name)
+  )
+]
+q5
+#Q5
+#rented film_ids
+rented_film_ids <- unique(
+  inventory[rental, on = "inventory_id", film_id]
+)
 source("Assignment3.R")
